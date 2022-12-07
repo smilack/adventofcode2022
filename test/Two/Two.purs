de970d01aa8b37d2b340b765c2c0f2e806f53774
Tom Smilack <tom@smilack.net>
2022-12-07 14:45:53 -0600

    Clean up imports in d2 tests

--- test/Two/Two.purs
+++ test/Two/Two.purs
@@ -2,16 +2,12 @@ module Test.AdventOfCode.Twenty22.Two
   ( main
   ) where
 
-import Prelude
+import Prelude (Unit, discard, ($))
 import AdventOfCode.Twenty22.Two
-import Data.String (split)
-import Data.String.Pattern (Pattern(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Test.QuickCheck ((===), Result)
-import Test.Spec (Spec, pending, describe, it)
+import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
-import Test.Spec.QuickCheck (quickCheck)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
 
